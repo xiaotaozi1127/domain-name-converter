b60db17c7b4e4d816dca8231dc0e024a1fe26992
taohui <htao@thoughtworks.com>
2022-04-01 14:19:57 +0800

    taohui: integrate swagger and enable swagger-ui

---
+++ src/main/java/com/scdt/assignment/scdtjavaassignment/configuration/SpringFoxConfig.java
@@ -0,0 +1,36 @@
+package com.scdt.assignment.scdtjavaassignment.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Domain Name Converter API")
+                .description("Domain name converter API reference for developers")
+                .contact(new Contact("taohui",
+                        "https://github.com/xiaotaozi1127/domain-name-converter",
+                        "xiaotaozi1127@hotmail.com"))
+                .version("1.0").build();
+    }
+
+}
