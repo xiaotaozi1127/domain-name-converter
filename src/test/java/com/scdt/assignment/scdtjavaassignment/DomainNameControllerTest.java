19ef0b0d91edf6d61a3ff8a967c39bc740edfb3f
taohui <htao@thoughtworks.com>
2022-04-01 09:24:37 +0800

    taohui: refactor encode api test

--- src/test/java/com/scdt/assignment/scdtjavaassignment/DomainNameControllerTest.java
+++ src/test/java/com/scdt/assignment/scdtjavaassignment/DomainNameControllerTest.java
@@ -16,16 +16,27 @@ public class DomainNameControllerTest {
 
     @Test
     public void should_encode_a_long_domain_name_to_a_short_domain_name() {
-        String longName = "https://www.baidu.com";
+        String longName1 = "https://www.baidu.com";
+        String longName2 = "https://www.sina.com";
 
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/domain/encode?longName={longName}",
-                String.class, longName);
+        ResponseEntity<String> responseEntity1 = restTemplate.getForEntity("/domain/encode?longName={longName}",
+                String.class, longName1);
+        ResponseEntity<String> responseEntity2 = restTemplate.getForEntity("/domain/encode?longName={longName}",
+                String.class, longName2);
 
-        HttpStatus statusCode = responseEntity.getStatusCode();
-        String body = responseEntity.getBody();
-        Assertions.assertEquals(HttpStatus.OK, statusCode);
-        Assertions.assertNotNull(body);
-        Assertions.assertTrue(body.startsWith("t.cn/"));
+        HttpStatus statusCode1 = responseEntity1.getStatusCode();
+        String body1 = responseEntity1.getBody();
+        Assertions.assertEquals(HttpStatus.OK, statusCode1);
+        Assertions.assertNotNull(body1);
+        Assertions.assertTrue(body1.startsWith("t.cn/"));
+
+        HttpStatus statusCode2 = responseEntity2.getStatusCode();
+        String body2 = responseEntity2.getBody();
+        Assertions.assertEquals(HttpStatus.OK, statusCode2);
+        Assertions.assertNotNull(body2);
+        Assertions.assertTrue(body2.startsWith("t.cn/"));
+
+        Assertions.assertNotEquals(body1, body2);
     }
 
     @Test
